96915b569ada82a2e8272ce5e758a6bde69ca15a
yuichi10 <y_084743@yahoo.co.jp>
2018-08-24 00:07:49 +0900

    prepare for file loading

--- lircd/reader.go
+++ lircd/reader.go
@@ -1,8 +1,21 @@
 package lircd
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+const (
+	beginRemote  = "begin remote"
+	endRemote    = "end remote"
+	beginRawCode = "begin raw_codes"
+	endRawCode   = "end raw_codes"
+	beginCodes   = "begin codes"
+	endCodes     = "end codes"
+	codeName     = "name"
 )
 
 type Lircd struct {
@@ -10,12 +23,21 @@ type Lircd struct {
 	action []string
 }
 
-func loadFile(filepath string) error {
-	f, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		return err
+func analyzeLircd(f *os.File) *Lircd {
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 	}
+	return &Lircd{}
+}
 
+func loadFile(filepath string, lircd chan *Lircd) {
+	f, err := os.Open(filepath)
+	// when filed to load file cause panic
+	if err != nil {
+		panic(err)
+	}
+	lircd <- analyzeLircd(f)
 }
 
 func LoadLircdConf(dirPath string) error {
@@ -23,5 +45,21 @@ func LoadLircdConf(dirPath string) error {
 	if err != nil {
 		return fmt.Errorf("failed to load lircd file: %s", err)
 	}
+	// var wg sync.WaitGroup
 
+	lircds := make([]*Lircd, 0, 5)
+	lircd := make(chan *Lircd, 4)
+	for _, file := range files {
+		if !file.IsDir() {
+			go loadFile(filepath.Join(dirPath, file.Name()), lircd)
+		}
+	}
+
+	for range files {
+		if l := <-lircd; l != nil {
+			lircds = append(lircds, l)
+		}
+	}
+	fmt.Println(lircds)
+	return nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/yuichi10/channels/config"
+	"github.com/yuichi10/channels/lircd"
 )
 
 func main() {
 	config.LoadOption()
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("failed to get working directory")
+	}
+	dir := filepath.Join(wd, "lircd.conf.d")
+	lircd.LoadLircdConf(dir)
 }

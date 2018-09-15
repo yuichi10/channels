71e0a34e06ff8cd2b8f79b81ad75270b4d0169a2
yuichi10 <y_084743@yahoo.co.jp>
2018-09-16 02:19:19 +0900

    lircd set http status

--- lircd/lircd.go
+++ lircd/lircd.go
@@ -26,12 +26,15 @@ func (l Lircd) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	paths := strings.FieldsFunc(r.URL.Path, f)
 	if len(paths) != 4 {
 		fmt.Println("came from invalid url:", r.URL.Path, "len:", len(paths))
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 	fmt.Println(irsend, sendOnce, paths[2], paths[3])
 	err := exec.Command(irsend, sendOnce, paths[2], paths[3]).Run()
 	if err != nil {
 		fmt.Println("failed to exec command:", err)
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	w.WriteHeader(http.StatusOK)
 }
